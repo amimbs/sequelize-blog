0bac675820e3f9bae55737b3b415bca4fc81d820
amimbs <andymimbs91@gmail.com>
2022-03-31 22:55:47 -0400

    All methods are functional on the server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const { response } = require('express');
+
 const express = require('express');
+const { Op } = require('@sequelize/core')
 
 // App Setup
 const app = express();
@@ -20,7 +21,7 @@ app.post('/app', (req, res) => {
     let body = req.body.body;
     let catagories = req.body.catagories;
 
-    models.blog_post.create({title: title, body: body, catagories: catagories}).then((post) => {
+    models.blog_post.create({ title: title, body: body, catagories: catagories }).then((post) => {
         res.json(post)
     })
 })
@@ -33,7 +34,48 @@ app.get('/app', (req, res) => {
 });
 
 // PUT Ability to update a post
+app.put('/app/:id', (req, res) => {
+    let id = parseInt(req.params.id);
 
+    models.blog_post.findByPk(id).then((post) => {
+        if (!post) {
+            res.json({ errors: 'Invalid ID' })
+        } else {
+            post.update({
+                title:req.body.title,
+                body:req.body.body,
+                catagories:req.body.catagories
+            })
+            res.json(post);
+        }
+    })
+});
+
+// GET Ability to filter posts based on their categories
+app.get('/app/:catagory', async(req, res) => {
+    let {catagory} = req.params
+    const data = await models.blog_post.findAll({
+        where: {
+            catagories: {
+                [Op.iLike]: `%${catagory}%`
+            }
+        }
+    })
+    // console.log(data)
+    res.json(data)
+});
+
+// DELETE Ability to delete a post
+app.delete('/app/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+
+    models.blog_post.findByPk(id).then((post) => {
+        if (post) {
+            post.destroy();
+        }
+        res.status(201).json({ result:`Id: ${id} deleted.`})
+    })
+});
 
 app.listen(8080, function () {
     console.log('Todo List App is now listening on port 8080...');
