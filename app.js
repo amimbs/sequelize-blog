e6379df2a8289e60c2a5af452cf753b57411cc47
amimbs <andymimbs91@gmail.com>
2022-03-31 21:15:32 -0400

    GET and POST methods are now functional

--- app.js
+++ app.js
@@ -14,11 +14,27 @@ const models = require('./models');
 // Template Engine Configuration
 app.set('view engine', 'ejs');
 
-// GET request
+// Ability to create a new post
+app.post('/app', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let catagories = req.body.catagories;
 
+    models.blog_post.create({title: title, body: body, catagories: catagories}).then((post) => {
+        res.json(post)
+    })
+})
+
+// GET Ability to view all posts
 app.get('/app', (req, res) => {
-    models.SeqTodo.findALL().then res => {
-        res.json({ : })
-    }
+    models.blog_post.findAll().then(all_posts => {
+        res.json(all_posts)
+    })
 });
 
+// PUT Ability to update a post
+
+
+app.listen(8080, function () {
+    console.log('Todo List App is now listening on port 8080...');
+});
---
+++ blog_post_data.js
@@ -0,0 +1,14 @@
+const blog_post = [
+    {
+        id: 1,
+        title: "Monday",
+        body: "Garfield comment",
+        catagoies: "Android",
+    },
+    {
+        id: 2,
+        title: "Tuesday",
+        body: "Tacos",
+        catagoies: "Python",
+    }
+]
